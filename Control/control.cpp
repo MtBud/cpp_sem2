b90aca90f0bb5b89453fd75be0fc52c77d5630d1
Matyáš Budský <budskmat@fit.cvut.cz>
2023-04-17 14:08:19 +0200

    control: Added todo comments and fixed formating

--- Control/control.cpp
+++ Control/control.cpp
@@ -19,53 +19,41 @@
 #include <functional>
 using namespace std;
 
-class CDate
-{
+class CDate{
   public:
-    //---------------------------------------------------------------------------------------------
-                             CDate                         ( int               y,
-                                                             int               m,
-                                                             int               d )
-      : m_Year ( y ),
-        m_Month ( m ),
-        m_Day ( d )
-    {
-    }
-    //---------------------------------------------------------------------------------------------
-    int                      compare                       ( const CDate     & x ) const
-    {
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cppcoreguidelines-narrowing-conversions"
+    CDate( int y, int m, int d ): m_Year (y), m_Month (m), m_Day (d){}
+#pragma clang diagnostic pop
+
+    int compare ( const CDate& x ) const{
       if ( m_Year != x . m_Year )
         return m_Year - x . m_Year;
       if ( m_Month != x . m_Month )
         return m_Month - x . m_Month;
       return m_Day - x . m_Day;
     }
-    //---------------------------------------------------------------------------------------------
-    int                      year                          ( void ) const 
-    {
+
+    int year () const{
       return m_Year;
     }
-    //---------------------------------------------------------------------------------------------
-    int                      month                         ( void ) const 
-    {
+
+    int month () const{
       return m_Month;
     }
-    //---------------------------------------------------------------------------------------------
-    int                      day                           ( void ) const 
-    {
+
+    int day () const{
       return m_Day;
     }
-    //---------------------------------------------------------------------------------------------
-    friend ostream         & operator <<                   ( ostream         & os,
-                                                             const CDate     & x )
-    {
+
+    friend ostream& operator << ( ostream& os, const CDate& x ){
       char oldFill = os . fill ();
-      return os << setfill ( '0' ) << setw ( 4 ) << x . m_Year << "-" 
-                                   << setw ( 2 ) << static_cast<int> ( x . m_Month ) << "-" 
-                                   << setw ( 2 ) << static_cast<int> ( x . m_Day ) 
+      return os << setfill ( '0' ) << setw ( 4 ) << x . m_Year << "-"
+                                     << setw ( 2 ) << static_cast<int> ( x . m_Month ) << "-"
+                                     << setw ( 2 ) << static_cast<int> ( x . m_Day )
                 << setfill ( oldFill );
     }
-    //---------------------------------------------------------------------------------------------
+
   private:
     int16_t                  m_Year;
     int8_t                   m_Month;
@@ -73,61 +61,95 @@ class CDate
 };
 #endif /* __PROGTEST__ */
 
-class CInvoice
-{
+// reprezentuje jednu fakturu
+class CInvoice{
   public:
-                             CInvoice                      ( const CDate     & date,
-                                                             const string    & seller,
-                                                             const string    & buyer,
-                                                             unsigned int      amount,
-                                                             double            vat );
-    CDate                    date                          ( void ) const;
-    string                   buyer                         ( void ) const;
-    string                   seller                        ( void ) const;
-    unsigned int             amount                        ( void ) const;
-    double                   vat                           ( void ) const;
+    /*    inicializace fakturu datem, jménem prodávající a kupující firmy, fakturovanou částkou a DPH.*/
+    CInvoice ( const CDate& date, const string& seller, const string& buyer,
+               unsigned int amount, double vat );
+
+    /*date, seller, buyer, amount, vat -- přístupové metody ke čtení jednotlivých složek faktury.*/
+    CDate date () const;
+    string buyer () const;
+    string seller () const;
+    unsigned int amount () const;
+    double vat () const;
   private:
     // todo
 };
 
-class CSortOpt
-{
+
+/*Třída CSortOpt určuje kritéria pro řazení. Pro řazení lze použít všechny složky faktury. Pokud například vytvoříme instanci:
+ * CSortOpt () . addKey ( CSortOpt::BY_AMOUNT, true ) . addKey ( CSortOpt::BY_SELLER, false )
+ * pak se řadí podle fakturované částky vzestupně (první řadicí kritérium) a pro stejné hodnoty fakturované částky
+ * se použije řazení podle jména prodávajícího sestupně (druhé řadicí kritérium). Pokud by ani takto nebylo pořadí
+ * jednoznačně určené, použije se jako řadicí kritérium pořadí zavedení faktury do registru. Rozhraní třídy CSortOpt je:*/
+class CSortOpt{
   public:
-    static const int         BY_DATE                       = 0;
-    static const int         BY_BUYER                      = 1;
-    static const int         BY_SELLER                     = 2;
-    static const int         BY_AMOUNT                     = 3;
-    static const int         BY_VAT                        = 4;
-                             CSortOpt                      ( void );
-    CSortOpt               & addKey                        ( int               key,
-                                                             bool              ascending = true );
+    static const int BY_DATE = 0;
+    static const int BY_BUYER = 1;
+    static const int BY_SELLER = 2;
+    static const int BY_AMOUNT = 3;
+    static const int BY_VAT = 4;
+    CSortOpt ();
+    CSortOpt& addKey ( int key, bool ascending = true );
   private:
     // todo
 };
 
-class CVATRegister
-{
+// registruje odeslané a přijaté faktury
+class CVATRegister{
   public:
-                             CVATRegister                  ( void );
-    bool                     registerCompany               ( const string    & name );
-    bool                     addIssued                     ( const CInvoice  & x );
-    bool                     addAccepted                   ( const CInvoice  & x );
-    bool                     delIssued                     ( const CInvoice  & x );
-    bool                     delAccepted                   ( const CInvoice  & x );
-    list<CInvoice>           unmatched                     ( const string    & company,
-                                                             const CSortOpt  & sortBy ) const;
+    //inicializuje prázdnou instanci registru,
+    CVATRegister ();
+
+    /*metoda zavede zadanou firmu do registru. Předané jméno je oficiální název firmy, toto jméno bude používané v
+     * exportech z registru. Návratovou hodnotou je indikátor úspěchu (true)/neúspěchu (false). Za neúspěch považujte,
+     * pokud v registru již existuje firma stejného jména. Při porovnávání jména firmy je registr docela tolerantní:
+
+    při porovnávání nerozlišuje malá a velká písmena,
+    při porovnávání neuvažuje nadbytečné mezery.
+
+    Tato pravidla jsou používána při zakládání nové firmy i vkládání / mazání faktur. Například názvy "My Company",
+     "mY CoMpAnY", " My Company " a " mY CoMPAnY " jsou považované za jednu firmu, ale názvy "My Company" a "MyCompany" označují dvě rozdílné firmy.*/
+    bool registerCompany ( const string& name );
+
+    /*metoda přidá fakturu do registru. Tuto metodu volá firma, která fakturu vydala. Návratovou hodnotou je příznak
+     * úspěch (true)/neúspěch (false). Za chybu je považováno, pokud prodávající / kupující ve faktuře nejsou
+     * registrované, prodávající a kupující je ta samá firma nebo pokud stejná faktura již byla pomocí addIssued
+     * zadaná (dvě faktury se musí lišit alespoň v jednom z: prodávající/kupující/datum/částka/DPH).*/
+    bool addIssued ( const CInvoice& x );
+
+    /*    metoda přidá fakturu do registru, tuto metodu volá firma, která fakturu přijala (kupující).
+     * Jinak se metoda chová stejně jako addIssued.*/
+    bool addAccepted ( const CInvoice& x );
+
+    /*metoda odebere fakturu z registru. Tuto metodu volá firma, která fakturu vydala a dříve zaregistrovala.
+     * Návratovou hodnotou je příznak úspěch (true)/neúspěch (false). Za chybu je považováno, pokud identická
+     * faktura nebyla dříve registrovaná metodou addIssued.*/
+    bool delIssued ( const CInvoice& x );
+
+    /*metoda odebere fakturu z registru. Tuto metodu volá firma, která fakturu přijala a dříve zaregistrovala.
+     * Návratovou hodnotou je příznak úspěch (true)/neúspěch (false). Za chybu je považováno, pokud identická
+     * faktura nebyla dříve registrovaná metodou addAccepted.*/
+    bool delAccepted ( const CInvoice& x );
+
+    /*metoda nalezne všechny faktury, které se týkají zadané firmy company a nebyly spárované (tedy byly registrované
+     * pouze pomocí addIssued nebo pouze pomocí addAccepted). Metoda vrátí seznam těchto faktur, faktury budou
+     * seřazené podle kritérií udaných sortOpt. Faktury vrácené touto metodou budou mít na místě názvu firmy
+     * "oficiální" název, tedy ten název, který byl zadán při registraci firmy metodou registerCompany.
+     * Tento oficiální název bude rovněž použit při řazení.*/
+    list<CInvoice> unmatched ( const string& company, const CSortOpt& sortBy ) const;
   private:
     // todo
 };
 
 #ifndef __PROGTEST__
-bool equalLists ( const list<CInvoice> & a, const list<CInvoice> & b )
-{
+bool equalLists ( const list<CInvoice> & a, const list<CInvoice> & b ){
   // todo
 }
 
-int main ( void )
-{
+int main (){
   CVATRegister r;
   assert ( r . registerCompany ( "first Company" ) );
   assert ( r . registerCompany ( "Second     Company" ) );
